d04e7eb2c2960eecacfce7543fa2f4208228b0ce
江流儿 <1098621287@qq.com>
2019-07-22 21:22:17 +0800

    提交代码

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -38,7 +38,7 @@ export const fetchHomeNavBar = <T>(data: T): AxiosPromise => {
 
 /**
  *
- * @param data 商品分类id 不传查找所有商品
+ * @param data 商品分类id 不传查找所有商品s
  */
 export const fetchGoods = (data?: object): AxiosPromise => {
   return fetch({
